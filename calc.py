bdfc0794ff4cb040fd200f87da85826810af8fa2
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2023-08-11 22:39:10 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,70 +1,26 @@
-def soma(a, b):
-    return a + b
-
-def subtracao(a, b):
-    return a - b
-
-def multiplicacao(a, b):
-    return a * b
-
-def divisao(a, b):
-    if b != 0:
-        return a / b
+def calc(a, b):
+    if a == b: return "a igual b"
     else:
-        return "Erro: divisão por zero"
-
-def potencia(a, b):
-    return a ** b
-
-def fatorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fatorial(n - 1)
+        if a > b:
+            res = a - b
+            print("a é maior")
+        else:
+            res = b - a
+            print("b é maior")
+        return res
 
 def fibonacci(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
+    if n <= 0: return []
+    elif n == 1: return [0]
+    elif n == 2: return [0, 1]
     else:
-        fib_series = [0, 1]
-        while len(fib_series) < n:
-            fib_series.append(fib_series[-1] + fib_series[-2])
-        return fib_series
+        fib = [0, 1]
+        while len(fib) < n:
+            fib.append(fib[-1] + fib[-2])
+        return fib
 
-print("Calculadora Expandida")
-print("1. Soma")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
-print("5. Potência")
-print("6. Fatorial")
-print("7. Sequência Fibonacci")
+a = int(input("Insira um número inteiro: "))
+b = int(input("Insira outro número inteiro: "))
 
-opcao = int(input("Escolha uma operação (1/2/3/4/5/6/7): "))
-
-if opcao in [1, 2, 3, 4, 5]:
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
-else:
-    num = int(input("Digite um número: "))
-
-if opcao == 1:
-    print("Resultado:", soma(num1, num2))
-elif opcao == 2:
-    print("Resultado:", subtracao(num1, num2))
-elif opcao == 3:
-    print("Resultado:", multiplicacao(num1, num2))
-elif opcao == 4:
-    print("Resultado:", divisao(num1, num2))
-elif opcao == 5:
-    print("Resultado:", potencia(num1, num2))
-elif opcao == 6:
-    print("Resultado:", fatorial(num))
-elif opcao == 7:
-    print("Resultado:", fibonacci(num))
-else:
-    print("Opção inválida")
+print("Resultado calc:", calc(a, b))
+print("Resultado fibonacci:", fibonacci(b))
