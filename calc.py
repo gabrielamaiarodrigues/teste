094566d939cca284c0a734cbcbbacbb2af7a826d
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2023-08-11 22:42:56 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,54 +1,31 @@
-def calc(a, b):
-    if a == b: return "a igual b"
+def calcular_media(lista_numeros):
+    total = 0
+    for i in range(len(lista_numeros)):
+        total += lista_numeros[i]
+    media = total / len(lista_numeros)
+    return media
+
+def verificar_paridade(numero):
+    if numero % 2 == 0:
+        return True
     else:
-        if a > b:
-            res = a - b
-            print("a é maior")
-        else:def fatorial(numero): return 1 if numero <= 1 else numero * fatorial(numero-1)
+        return False
 
-def fibonacci(n): return [0] if n == 1 else [0, 1] if n == 2 else fibonacci(n-1).append(fibonacci(n-1)[-1] + fibonacci(n-1)[-2]) or fibonacci(n-1)
+def main():
+    quantidade_numeros = int(input("Digite a quantidade de números: "))
+    numeros = []
+    for i in range(quantidade_numeros):
+        num = int(input("Digite o {}º número: ".format(i+1)))
+        numeros.append(num)
 
-def operacao(opc, x, y): return x + y if opc == 1 else x - y if opc == 2 else x * y if opc == 3 else x / y if opc == 4 else "Operação inválida"
+    media = calcular_media(numeros)
+    print("A média dos números é:", round(media, 2))
 
-def menu():
-    print("Calculadora")
-    print("1. Soma")
-    print("2. Subtracao")
-    print("3. Multiplicacao")
-    print("4. Divisao")
-    print("5. Fatorial")
-    print("6. Fibonacci")
-    opc = int(input("Escolha uma operação (1/2/3/4/5/6): "))
-    if opc in [1, 2, 3, 4]:
-        num1 = float(input("Insira o primeiro número: "))
-        num2 = float(input("Insira o segundo número: "))
-        resultado = operacao(opc, num1, num2)
-        print("Resultado:", resultado)
-    elif opc in [5, 6]:
-        num = int(input("Insira um número inteiro: "))
-        resultado = fatorial(num) if opc == 5 else fibonacci(num)
-        print("Resultado:", resultado)
+    num_verificar = int(input("Digite um número para verificar se é par: "))
+    if verificar_paridade(num_verificar):
+        print("O número é par.")
     else:
-        print("Operação inválida")
+        print("O número é ímpar.")
 
-menu()
-
-            res = b - a
-            print("b é maior")
-        return res
-
-def fibonacci(n):
-    if n <= 0: return []
-    elif n == 1: return [0]
-    elif n == 2: return [0, 1]
-    else:
-        fib = [0, 1]
-        while len(fib) < n:
-            fib.append(fib[-1] + fib[-2])
-        return fib
-
-a = int(input("Insira um número inteiro: "))
-b = int(input("Insira outro número inteiro: "))
-
-print("Resultado calc:", calc(a, b))
-print("Resultado fibonacci:", fibonacci(b))
+if __name__ == "__main__":
+    main()
