33b0ed72ac1f50dd4f9a3072dc61aecb08fc5631
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2023-08-21 11:09:21 -0300

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,12 +1,16 @@
-public class Calculator {
+public class Soma {
     public int add(int a, int b) {
         return a + b;
     }
+}
 
+public class Subt {
     public int subtract(int a, int b) {
         return a - b;
     }
+}
 
+public class Mult {
     public int multiply(int a, int b) {
         return a * b;
     }
